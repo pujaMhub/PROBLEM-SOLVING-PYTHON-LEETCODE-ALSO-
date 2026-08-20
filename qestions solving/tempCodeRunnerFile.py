935f19bae77eda35c999354dba4e98ae723e7f69
PujaMhub <pujamaity9038@gmail.com>
2026-08-20 23:18:09 +0530

    updated1

--- qestions solving/tempCodeRunnerFile.py
+++ qestions solving/tempCodeRunnerFile.py
@@ -1,13 +1,16 @@
-# method 3 bettter
-def second(arr):
-    largest=float("-inf")
-    secondlarge=float("-inf")
+
+# method 4 with optimized
+def secondlarge(arr):
+    large=float("-inf")
+    second=float("-inf")
     n=len(arr)
     for i in range(0,n):
-        largest=max(largest,arr[i])
-    for i in range(0,n):
-        if arr[i]>secondlarge and arr[i]!=largest: #already secondlargest dhorai ache
-            secondlarge=arr[i]
-    return secondlarge
-arr=[7,5,4,6,22]
-print(second(arr))
+        if arr[i]>large:# if arr[i] is big than arge second will update in large and large will update with value arr[i]
+            second=large
+            large=arr[i]
+        elif arr[i]>second and arr[i]!=large:#if again second is big than arr[i] second variable will updated to arr[i] but
+            # it must be not equal to the previous large number
+            second=arr[i]
+    return second
+arr=[453,3,2,4,390]
+print(secondlarge(arr))
