49aaa14738391d1b9ca4cd8a81408bb13a6a71e6
PujaMhub <pujamaity9038@gmail.com>
2026-09-04 22:48:59 +0530

    new add

--- qestions solving/tempCodeRunnerFile.py
+++ qestions solving/tempCodeRunnerFile.py
@@ -1,12 +1,10 @@
-def dup(arr):
-    n=len(arr)
-    freq_map={}
+def num(n):
+    m=len(n)
+    f={}
     for i in range(0,n):
-        freq_map[arr[i]]=0
-    j=0
-    for k in freq_map:
-        arr[j]=k
-        j+=1
-    return j
-arr=[1,2,3,3,4,5,7,7,8,10]
-print(dup(arr))
+        for j in range(i+1,n):
+            if n[i]!=n[j]:
+                f[n[i]]=0
+    return f
+n=[1,1,2,2,2,3,4,5,6]
+print(num(n))
