32c66054f8050ba0bdae549f4eba51e0a6748d4c
PujaMhub <pujamaity9038@gmail.com>
2026-09-09 18:48:23 +0530

    new again

--- qestions solving/pairsmall.py
+++ qestions solving/pairsmall.py
@@ -3,13 +3,16 @@ def pair(arr):
     n=len(arr)
 
     small=float('inf')
+    si=sj=0
     for i in range(n):
         for j in range(i+1,n):
             total=arr[i]+arr[j]
 
             if total<small:
                 small=total
-    return small
+                si=i
+                sj=j
+    return small,si,sj
 arr=[2,1,5,6,7]
 print(pair(arr))
  
